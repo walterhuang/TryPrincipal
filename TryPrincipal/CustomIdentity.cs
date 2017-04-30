0e9f47815b6b03dc3bcebc7f526985b894a74932
walterhuang <isponder@gmail.com>
2017-04-30 02:20:00 -0700

    Add project files.

---
+++ TryPrincipal/CustomIdentity.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryPrincipal
+{
+    public class CustomIdentity : IIdentity
+    {
+        public CustomIdentity(string name, string email, string[] roles)
+        {
+            Name = name;
+            Email = email;
+            Roles = roles;
+        }
+
+        public string Email { get; set; }
+        public string[] Roles { get; set; }
+
+        public string AuthenticationType
+        {
+            get
+            {
+                return "Custom authentication";
+            }
+        }
+
+        public bool IsAuthenticated
+        {
+            get
+            {
+                return !string.IsNullOrEmpty(Name);
+            }
+        }
+
+        public string Name
+        {
+            get; private set;
+        }
+    }
+
+    public class AnonymousIdentity : CustomIdentity
+    {
+        public AnonymousIdentity() : base("", "", new string[] { })
+        {
+
+        }
+    }
+}
---
+++ TryPrincipal/CustomPrincipal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryPrincipal
+{
+    public class CustomPrincipal : IPrincipal
+    {
+        private CustomIdentity _identity;
+        public CustomIdentity Identity
+        {
+            get
+            {
+                return _identity ?? new AnonymousIdentity();
+            }
+            set { _identity = value; }
+        }
+
+        IIdentity IPrincipal.Identity
+        {
+            get
+            {
+                return Identity;
+            }
+        }
+
+        public bool IsInRole(string role)
+        {
+            return _identity.Roles.Contains(role);
+        }
+    }
+}
